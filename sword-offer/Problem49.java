e037116f9df34d7c6436ce3126a14012073cf828
wuleiaty <1046828876@qq.com>
2020-03-10 21:12:31 +0800

    丑数

---
+++ sword-offer/Problem49.java
@@ -0,0 +1,22 @@
+package Problem49;
+
+public class Problem49 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().nthUglyNumber(10));
+    }
+}
+
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int p2 = 0, p3 = 0, p5 = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.min(Math.min(dp[p2] * 2, dp[p3] * 3), dp[p5] * 5);
+            if (dp[i] == dp[p2] * 2) p2++;
+            if (dp[i] == dp[p3] * 3) p3++;
+            if (dp[i] == dp[p5] * 5) p5++;
+        }
+        return dp[n - 1];
+    }
+}
