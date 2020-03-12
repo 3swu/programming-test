ca29189a695e896da4e2d9dfa1d3e18c69890d10
wuleiaty <1046828876@qq.com>
2020-03-12 20:46:02 +0800

    二叉树的深度

---
+++ sword-offer/Problem55_I.java
@@ -0,0 +1,25 @@
+package Problem55_I;
+
+public class Problem55_I {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x;}
+}
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
