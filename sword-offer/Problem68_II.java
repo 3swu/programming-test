84064201a03e4c9ea5d3832f6c69bdb83e7c2847
wuleiaty <1046828876@qq.com>
2020-03-14 21:43:06 +0800

    二叉树的最近公共祖先

---
+++ sword-offer/Problem68_II.java
@@ -0,0 +1,31 @@
+package Problem68_II;
+
+public class Problem68_II {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x;}
+}
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) return root;
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+        if (l == null) return r;
+        if (r == null) return l;
+        return root;
+    }
+}
